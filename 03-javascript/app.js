0f6a3ce06321e4aa00f1f4c44d8f7efb19a77273
Ghulam Mustfa <mustfafsd41@gmail.com>
2025-05-05 14:47:20 +0500

    practice

--- 03-javascript/app.js
+++ 03-javascript/app.js
@@ -1,2 +1,11 @@
 console.log('hello world');
 console.log("%cHello, World", "color: blue; font-size: 40px");
+
+
+
+var cubes = 'ABCDEFG';
+//styling console output using CSS with a %c format specifier
+for (var i = 0; i < 7; i++) {
+    var styles = "font-size: 40px; border-radius: 10px; border: 1px solid blue; background: pink; color: purple";
+    console.log("%c" + cubes[i], styles)
+}
